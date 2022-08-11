db0964a172ed54c2be61e35c817db1b4e6b6b9d0
tinho1123 <tinhoefaela@gmail.com>
2022-08-11 14:57:48 -0300

    feat: create Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './styles.css'
+
+const Header = () => {
+  return (
+    <div id='header'>
+      <div className='headerContainer'>
+        <div className='header__myName'>
+          <h1>
+            Wellington Carvalho
+          </h1>
+        </div>
+        <div className='header__list'>
+          <ul>
+            <li><a href="#sobreMim">Sobre mim</a></li>
+            <li><a href="#stacks">Stacks</a></li>
+            <li><a href="#projetos">Projetos</a></li>
+            <li><a href="#contatos">Contatos</a></li>
+
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/index.tsx
@@ -0,0 +1,7 @@
+import Header from './Header/Header'
+import SobreMim from './SobreMim/SobreMim'
+
+export {
+    Header,
+    SobreMim
+}
