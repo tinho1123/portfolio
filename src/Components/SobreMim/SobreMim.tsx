d3de0d79696a4ad5615050f9f7a7a356806461f2
tinho1123 <tinhoefaela@gmail.com>
2022-08-11 15:26:04 -0300

    header responsive

--- src/Components/SobreMim/SobreMim.tsx
+++ src/Components/SobreMim/SobreMim.tsx
@@ -7,6 +7,9 @@ const SobreMim = () => {
       <div className='sobreMimContainer'>
         <div className='sobreMim_information'>
           <h1>Hi there </h1>
+          <h1>Me chamo Wellington</h1>
+          <p>Sou um apaixonado por jogos e tecnologia!</p>
+          <p></p>
         </div>
         <div className='sobreMim_profile' >
           <h1>sadasd</h1>
